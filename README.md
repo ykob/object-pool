7603e47d6d51790a1e5957341139753134b4e884
yoichi kobayashi <info@tplh.net>
2015-09-09 22:22:08 +0900

    [add] gulp set を導入。

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # object-pool
+
+オブジェクトプールの実験です。  
+一般的なパーティクルのアニメーションを、オブジェクトプールを用いて実装してみます。
+
+## npm install
+
+```
+npm i browser-sync browserify gulp gulp-ruby-sass gulp-sequence require-dir vinyl-source-stream watchify
+```
---
+++ src/js/force.js
@@ -0,0 +1,31 @@
+var Vector2 = require('./vector2');
+
+var exports = {
+  friction: function(vector, value) {
+    var force = vector.clone();
+    force.multScalar(-1);
+    force.normalize();
+    force.multScalar(value);
+    return force;
+  },
+  drag: function(vector, value) {
+    var force = vector.clone();
+    force.multScalar(-1);
+    force.normalize();
+    force.multScalar(vector.length() * value);
+    return force;
+  },
+  hook: function(v_velocity, v_anchor, k) {
+    var force = v_velocity.clone().sub(v_anchor);
+    var distance = force.length();
+    if (distance > 0) {
+      force.normalize();
+      force.multScalar(-1 * k * distance);
+      return force;
+    } else {
+      return new Vector2();
+    }
+  }
+};
+
+module.exports = exports;
